c053ef2ea7ae9f99bc5f882994dc9dbfdfddac1f
e7160u <valenino.lambert94@gmail.com>
2025-12-11 17:03:56 +0100

    ajout profil.vue

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Profile from '../views/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: Profile
     }
   ]
 })
---
+++ vue/src/views/Profile.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div class="profile">
+    <h1>Profil</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
