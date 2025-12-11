37da9ada811f591fb225aeb9f831b2d70188fb6d
Paul5400 <paul.andrieu54@gmail.com>
2025-12-11 17:43:22 +0100

    formulaire

--- vue/src/views/Profile.vue
+++ vue/src/views/Profile.vue
@@ -1,11 +1,26 @@
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      user: null
+    }
+  }
+}
 </script>
 
 <template>
-  <div class="profile">
-    <h1>Profil</h1>
+  <div>
+    <h1>Mon Profil</h1>
+    <form>
+      <div>
+        <label for="surnom">Surnom :</label>
+        <input id="surnom" v-model="user.surnom" type="text" />
+      </div>
+      <div>
+        <label for="email">Email :</label>
+        <input id="email" v-model="user.email" type="email" />
+      </div>
+      <button type="submit">Enregistrer</button>
+    </form>
   </div>
 </template>
-
-<style scoped>
-</style>
